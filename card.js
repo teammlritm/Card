b66a91ebefc3960f6016b1c1ef879d204493dd50
teammlritm <227y1a6613@mlritm.ac.in>
2025-02-19 14:10:34 +0530

    card.js

---
+++ card.js
@@ -0,0 +1,10 @@
+let button=document.getElementById("btn")
+let c1=document.getElementById("c1")
+let c2=document.getElementById("c2")
+let c3=document.getElementById("c3")
+
+function clicked(){
+    c1.classList.toggle("c11")
+    c2.classList.toggle("c22")
+    c3.classList.toggle("c33")
+}
